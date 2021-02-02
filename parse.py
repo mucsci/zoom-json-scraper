ca1518fa14c6e327aeb0050ec163834862c5155d
Will Killian <william.killian@millersville.edu>
2021-02-02 16:11:56 -0500

    Feature: now limits end time to when faculty member is present
    This may be useful if you wanted to manually change your duration to
    the specified class time (in seconds). I may eventually add a command
    line option to do that.
    
    Otherwise, general code cleanup and reorganization helps a lot.

--- parse.py
+++ parse.py
@@ -9,22 +9,36 @@ def read_file(name):
     with open(name) as f:
         return json.load(f)
 
-prof = sys.argv[1]
-data = read_file(sys.argv[2])
+def calc_durations(data, **kwargs):
+    prof, start, end = kwargs['prof'], kwargs['start'], kwargs['end']
+    durations = defaultdict(int)
+    for entry in data['attendees']:
+        if entry['email'] == prof:
+            continue
+        if entry['join'] < start:
+            entry['duration'] -= (start - entry['join'])
+            entry['join'] = start
+        if entry['join'] + entry['duration'] > end:
+            entry['duration'] = end - entry['join']
+        durations[entry['name']] += entry['duration']
+    return durations
 
-start = min(e['join'] for e in data['attendees'] if e['email'] == prof)
+def make_report(durations):
+    xbar = mean(t for _,t in durations.items())
+    s = pstdev((t for _,t in durations.items()), xbar)
+    for e, x in durations.items():
+        z = (x - xbar) / s
+        status = 'L' if (x - xbar) / s < -1.0 else 'P'
+        yield (status,e,z)
 
-durations = defaultdict(int)
-for entry in data['attendees']:
-    if entry['email'] == prof:
-        continue
-    if (entry['join'] < start):
-        entry['duration'] -= (start - entry['join'])
-    durations[entry['name']] += entry['duration']
+def main(prof, filename):
+    data = read_file(filename)
+    start = min(e['join'] for e in data['attendees'] if e['email'] == prof)
+    end = max(e['join'] + e['duration'] for e in data['attendees'] if e['email'] == prof)
+    durations = calc_durations(data, prof=prof, start=start, end=end)
+    zscores = sorted(x for x in make_report(durations))
+    for status,uid,score in zscores:
+        print(f"{status} {uid:30} [{durations[uid]/60:3.0f} min] ({score:5.2f})")
 
-xbar = mean(t for _,t in durations.items())
-s = pstdev((t for _,t in durations.items()), xbar)
-zscores = list((e,(x - xbar) / s) for e,x in durations.items())
-zscores.sort(key=lambda x: x[0])
-for uid,score in zscores:
-    print(f"{'L' if score < -1.0 else 'P'} {uid:30} [{durations[uid]/60:3.0f} min] ({score:5.2f})")
+if __name__ == '__main__':
+    main(*sys.argv[1:])
